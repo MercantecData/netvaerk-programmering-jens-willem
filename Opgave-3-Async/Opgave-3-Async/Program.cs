6f7b163fce2f56cd081bb1d477204bbe2292d885
william <william_kammer@live.dk>
2019-12-10 13:20:28 +0100

    OPG 3 start

---
+++ Opgave-3-Async/Opgave-3-Async/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Opgave_3_Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
