f33bf72db2ecbff4cd5ec3c6bd42da6ae3375bfe
william <william_kammer@live.dk>
2019-12-10 14:03:36 +0100

    OPG 3
    Program written mostly along the manual

--- Opgave-3-Async/Opgave-3-Async/Program.cs
+++ Opgave-3-Async/Opgave-3-Async/Program.cs
@@ -9,7 +9,57 @@ namespace Opgave_3_Async
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Server();
+            Client();
+        }
+        static void Client()
+        {
+            TcpClient client = new TcpClient();
+            int port = 13356;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            client.Connect(endPoint);
+
+            NetworkStream stream = client.GetStream();
+            ReceiveMessage(stream);
+
+            string text = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+
+            stream.Write(buffer, 0, buffer.Length);
+
+            client.Close();
+        }
+        static async void Server()
+        {
+            int port = 13356;
+            IPAddress ip = IPAddress.Any;
+            IPEndPoint localEndPoint = new IPEndPoint(ip, port);
+
+            TcpListener listener = new TcpListener(localEndPoint);
+
+            listener.Start();
+
+            Console.WriteLine("Awaiting Clients");
+            TcpClient client = await listener.AcceptTcpClientAsync();
+
+            NetworkStream stream = client.GetStream();
+            ReceiveMessage(stream);
+
+            Console.Write("write your message here: ");
+            string text = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+
+            stream.Write(buffer, 0, buffer.Length);
+            Console.ReadKey();
+        }
+        static async void ReceiveMessage(NetworkStream stream)
+        {
+            byte[] buffer = new byte[256];
+            int numberOfBytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+            string receivedMessage = Encoding.UTF8.GetString(buffer, 0, numberOfBytesRead);
+            Console.Write("\n" + receivedMessage);
         }
     }
 }
