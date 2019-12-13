13c9f167371a1ae2d81bdf0a6459e65287d2ca80
Jens <jens.e.bouwens@gmail.com>
2019-12-13 08:45:10 +0100

    something has been done

---
+++ MultiClient/MultiClient/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace MultiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public List<TcpClient> clients = new List<TcpClient>();
+        public MyServer()
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 13376;
+            TcpListener listener = new TcpListener(ip, port);
+            listener.Start();
+
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+
+            ReceiveMessages(stream);
+
+            bool isRunning = true;
+            while (isRunning)
+            {
+                //Send msg
+                Console.Write("Write message: ");
+                string text = Console.ReadLine();
+                byte[] buffer = Encoding.UTF8.GetBytes(text);
+                foreach(TcpClient client in clients) {
+                    client.GetStream().Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+        public async void ReceiveMessages(NetworkStream stream)
+        {
+            byte[] buffer = new byte[256];
+            bool isRunning = true;
+            while (isRunning)
+            {
+                int read = await stream.ReadAsync(buffer, 0, buffer.Length);
+                string text = Encoding.UTF8.GetString(buffer, 0, read);
+                Console.WriteLine("client writes: " + text);
+            }
+        }
+        public async void AcceptClients(TcpListener listener)
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                clients.Add(client);
+                NetworkStream stream = client.GetStream();
+                ReceiveMessages(stream);
+            }
+        }
+    }
+}
